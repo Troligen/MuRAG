3f1dd6462c460c607a65e1bbabece97b1134c500
troligen <utvecklarjesper@gmail.com>
2024-10-24 16:57:50 +0200

    Created a prompts folder and json file, refactored the entire pipeline function to a class that inherits from StateGraph

--- rag/generator.py
+++ rag/generator.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import List
 
 from langchain.load import dumps, loads
 from langchain.schema import Document
@@ -7,7 +7,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END, START, StateGraph
-from pydantic import BaseModel, Field, validate_call
+from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 
 
@@ -22,7 +22,6 @@ class GradeDocuments(BaseModel):
 class GraphState(TypedDict):
     """
     Represents the state of our graph.
-
     Attributes:
         question: question
         generation: LLM generation
@@ -37,33 +36,50 @@ class GraphState(TypedDict):
 
 
 class Prompts(BaseModel):
-    grader_system: str
-    rewrite_system: str
-    template: str
+    """
+    Dictiornary Keys for the prompts:
+
+    grader_template
+    rewrite_template
+    generator_template
+    rag_fusion_template
+    """
+
+    grader_template: str
+    rewrite_template: str
+    generator_template: str
     rag_fusion_template: str
 
 
-@validate_call
 class Pipeline(StateGraph):
     def __init__(
         self,
-        state,
+        retriever,
         prompts: Prompts,
-        web_search_tool=TavilySearchResults(max_results=5, search_depth="advanced"),
+        state=GraphState,
+        web_search_tool=None,
     ):
         super().__init__(state)
+
+        if web_search_tool is None:
+            web_search_tool = TavilySearchResults(
+                max_results=5,
+                search_depth="advanced",
+            )
+
+        self.retriever = retriever
 
         self.llm_4 = ChatOpenAI(model="gpt-4o-mini", temperature=1)
         self.llm_3 = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
         self.llm_f = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
-        self.prompts = prompts
+        self.prompts = Prompts(**prompts)  # pyright: ignore
         self.web_search_tool = web_search_tool
         self.structured_llm_grader = self.llm_f.with_structured_output(GradeDocuments)
 
         self.grade_prompt = ChatPromptTemplate.from_messages(
             [
-                ("system", self.prompts.grader_system),
+                ("system", self.prompts.grader_template),
                 (
                     "human",
                     "Retrieved document: \n\n {document} \n\n User question: {question}",
@@ -71,7 +87,38 @@ class Pipeline(StateGraph):
             ]
         )
 
-    def reciprocal_rank_fusion(self, results: list[list], k=60):
+        self.retrieval_grader = self.grade_prompt | self.structured_llm_grader
+
+        self.re_write_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", self.prompts.rewrite_template),
+                (
+                    "human",
+                    "Here is the initial question: \n\n {question} \n Formulate an improved question.",
+                ),
+            ]
+        )
+
+        self.prompt_rag_fusion = ChatPromptTemplate.from_template(
+            self.prompts.rag_fusion_template
+        )
+
+        self.question_rewriter = self.re_write_prompt | self.llm_f | StrOutputParser()
+
+        self.generate_queries = (
+            self.prompt_rag_fusion
+            | self.llm_3
+            | StrOutputParser()
+            | (lambda x: x.split("\n"))
+        )
+
+        self.prompt_generator = ChatPromptTemplate.from_template(
+            self.prompts.generator_template
+        )
+
+        self.rag_chain = self.prompt_generator | self.llm_4 | StrOutputParser()
+
+    def _reciprocal_rank_fusion(self, results: list[list], k=60):
         """
         Reciprocal_rank_fusion that takes multiple lists of ranked documents
         and an optional parameter k used in the RRF formula
@@ -104,72 +151,7 @@ class Pipeline(StateGraph):
 
         return reranked_results
 
-
-def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
-    llm_4 = ChatOpenAI(model="gpt-4o-mini", temperature=1)
-    llm_3 = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-    llm_f = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-
-    structured_llm_grader = llm_f.with_structured_output(GradeDocuments)
-
-    web_search_tool = TavilySearchResults(
-        max_results=5,
-        search_depth="advanced",
-    )
-
-    grader_system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
-    If the document contains keyword(s) or semantic meaning related to the question, grade it as relevant. \n
-    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
-
-    grade_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", grader_system),
-            (
-                "human",
-                "Retrieved document: \n\n {document} \n\n User question: {question}",
-            ),
-        ]
-    )
-
-    retrieval_grader = grade_prompt | structured_llm_grader
-
-    rewrite_system = """You a question re-writer that converts an input question to a better version that is optimized \n 
-     for web search. Look at the input and try to reason about the underlying semantic intent / meaning."""
-    re_write_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", rewrite_system),
-            (
-                "human",
-                "Here is the initial question: \n\n {question} \n Formulate an improved question.",
-            ),
-        ]
-    )
-
-    question_rewriter = re_write_prompt | llm_f | StrOutputParser()
-
-    template = """Use the following pieces of context to answer the question at the end. 
-    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-    {context}
-
-    Question: {question}
-    Helpful Answer:"""
-
-    rag_fusion_template = """You are a helpful assistant that generates multiple search queries based of a single input query. \n
-    Generate multiple search queries related to: {question} \n
-    output: (4 queries):
-    """
-
-    prompt_rag_fusion = ChatPromptTemplate.from_template(rag_fusion_template)
-
-    generate_queries = (
-        prompt_rag_fusion | llm_3 | StrOutputParser() | (lambda x: x.split("\n"))
-    )
-
-    prompt = ChatPromptTemplate.from_template(template)
-
-    rag_chain = prompt | llm_4 | StrOutputParser()
-
-    def retrieve(state):
+    def _retrieve(self, state):
         """
         Retrieve documents
 
@@ -183,15 +165,15 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
         question = state["question"]
 
         # Retrieval
-        queries = generate_queries.invoke({"question": question})
+        queries = self.generate_queries.invoke({"question": question})
         documents = []
         for query in queries:
-            results = retriever.invoke(query)
+            results = self.retriever.invoke(query)
             documents.append(results)
-        reranked_documents = reciprocal_rank_fusion(documents)
+        reranked_documents = self._reciprocal_rank_fusion(documents)
         return {"documents": reranked_documents, "question": question}
 
-    def generate(state):
+    def _generate(self, state):
         """
         Generate answer
 
@@ -206,14 +188,14 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
         documents = state["documents"]
 
         # RAG generation
-        generation = rag_chain.invoke({"context": documents, "question": question})
+        generation = self.rag_chain.invoke({"context": documents, "question": question})
         return {
             "documents": generation,
             "question": question,
             "generation": generation,
         }
 
-    def grade_documents(state):
+    def _grade_documents(self, state):
         """
         Determines whether the retrieved documents are relevant to the question.
 
@@ -232,7 +214,7 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
         filtered_docs = []
         web_search = "No"
         for d in documents:
-            score = retrieval_grader.invoke({"question": question, "document": d})
+            score = self.retrieval_grader.invoke({"question": question, "document": d})
             grade = score.binary_score  # pyright: ignore
             if grade == "yes":
                 print("---GRADE: DOCUMENT RELEVANT---")
@@ -247,7 +229,7 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
             "web_search": web_search,
         }
 
-    def transform_query(state):
+    def _transform_query(self, state):
         """
         Transform the query to produce a better question.
 
@@ -263,10 +245,10 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
         documents = state["documents"]
 
         # Re-write question
-        better_question = question_rewriter.invoke({"question": question})
+        better_question = self.question_rewriter.invoke({"question": question})
         return {"documents": documents, "question": better_question}
 
-    def web_search(state):
+    def _web_search(self, state):
         """
         Web search based on the re-phrased question.
 
@@ -282,14 +264,14 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
         documents = state["documents"]
 
         # Web search
-        docs = web_search_tool.invoke({"query": question})
+        docs = self.web_search_tool.invoke({"query": question})
         web_results = "\n".join([d["content"] for d in docs])
         web_results = Document(page_content=web_results)
         documents.append(web_results)
 
         return {"documents": documents, "question": question}
 
-    def decide_to_generate(state):
+    def _decide_to_generate(self, state):
         """
         Determines whether to generate an answer, or re-generate a question.
 
@@ -317,31 +299,31 @@ def setup_rag_pipeline(retriever, reciprocal_rank_fusion):
             print("---DECISION: GENERATE---")
             return "generate"
 
-    workflow = StateGraph(GraphState)
+    def compile_graph(self):
 
-    # Define the nodes
-    workflow.add_node("retrieve", retrieve)  # retrieve
-    workflow.add_node("grade_documents", grade_documents)  # grade documents
-    workflow.add_node("generate", generate)  # generatae
-    workflow.add_node("transform_query", transform_query)  # transform_query
-    workflow.add_node("web_search_node", web_search)  # web search
+        # Define the nodes
+        self.add_node("retrieve", self._retrieve)  # retrieve
+        self.add_node("grade_documents", self._grade_documents)  # grade documents
+        self.add_node("generate", self._generate)  # generatae
+        self.add_node("transform_query", self._transform_query)  # transform_query
+        self.add_node("web_search_node", self._web_search)  # web search
 
-    # Build graph
-    workflow.add_edge(START, "retrieve")
-    workflow.add_edge("retrieve", "grade_documents")
-    workflow.add_conditional_edges(
-        "grade_documents",
-        decide_to_generate,
-        {
-            "transform_query": "transform_query",
-            "generate": "generate",
-        },
-    )
-    workflow.add_edge("transform_query", "web_search_node")
-    workflow.add_edge("web_search_node", "generate")
-    workflow.add_edge("generate", END)
+        # Build graph
+        self.add_edge(START, "retrieve")
+        self.add_edge("retrieve", "grade_documents")
+        self.add_conditional_edges(
+            "grade_documents",
+            self._decide_to_generate,
+            {
+                "transform_query": "transform_query",
+                "generate": "generate",
+            },
+        )
+        self.add_edge("transform_query", "web_search_node")
+        self.add_edge("web_search_node", "generate")
+        self.add_edge("generate", END)
 
-    # Compile
-    app = workflow.compile()
+        # Compile
+        app = self.compile()
 
-    return app
+        return app
